7ba1336ba487594ec86b3fb3a0ba24c736097ee8
bijay <bijay>
2023-10-09 20:56:41 -0400

    Added entity for token

---
+++ Docsie.Data/Entities/TokenEntity.cs
@@ -0,0 +1,10 @@
+﻿using Docsie.Data.Entities.Base;
+
+namespace Docsie.Data.Entities
+{
+    public class TokenEntity: Entity
+    {
+        public string TenantId { get; set; }
+        public string Token { get; set; }
+    }
+}

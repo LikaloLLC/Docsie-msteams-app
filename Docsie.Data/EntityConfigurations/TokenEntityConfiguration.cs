988a8cd5bdd33d96eb02e0ccc9ee4f776abf4aa1
bijay <bijay>
2023-10-09 20:56:54 -0400

    Configured table for token

---
+++ Docsie.Data/EntityConfigurations/TokenEntityConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Docsie.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Docsie.Data.EntityConfigurations
+{
+    public class TokenEntityConfiguration : IEntityTypeConfiguration<TokenEntity>
+    {
+        public void Configure(EntityTypeBuilder<TokenEntity> builder)
+        {
+            builder.ToTable("Token", "dbo");
+            builder.HasKey(t => t.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd().IsRequired();
+            builder.Property(x => x.TenantId).IsRequired();
+            builder.Property(x => x.Token).IsRequired();
+        }
+    }
+}

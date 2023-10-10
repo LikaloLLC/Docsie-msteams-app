a248ab1b3c5157a56c6bb371df6a023a42e823fb
bijay <bijay>
2023-10-09 20:57:22 -0400

    Added generic repos for EF

---
+++ Docsie.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,41 @@
+﻿using Docsie.Data.Entities.Base;
+using Docsie.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Docsie.Data.Core.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : Entity
+    {
+
+        protected ApplicationDbContext _dbContext;
+
+        public GenericRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public async Task<T> GetFirstOrDefaultAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbContext.Set<T>().Where(predicate).FirstOrDefaultAsync();
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+            return entity;
+        }
+
+        public T UpdateAsync(T entity)
+        {
+            _dbContext.Set<T>().Update(entity);
+            return entity;
+        }
+
+        public T DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            return entity;
+        }
+    }
+}
---
+++ Docsie.Data/Repositories/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace Docsie.Data.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetFirstOrDefaultAsync(Expression<Func<T, bool>> predicate);
+        Task<T> AddAsync(T entity);
+        T UpdateAsync(T entity);
+        T DeleteAsync(T entity);
+    }
+}

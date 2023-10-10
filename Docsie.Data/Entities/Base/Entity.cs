905a3ea19999c0906c1c426aff35b0c66d17319d
bijay <bijay>
2023-10-09 20:56:30 -0400

    Added Entity base class

---
+++ Docsie.Data/Entities/Base/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Docsie.Data.Entities.Base
+{
+    public class Entity
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        [Column(Order = 1)]
+        public string Id { get; set; }
+    }
+}
